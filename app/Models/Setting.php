1d6f6fe30213f93a7a36058c767c2bc2ffc68133
emilsonsn <emilsonsn2@gmail.com>
2024-12-02 10:03:04 -0300

    fix: ajusta retorno de logo da imagem

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -27,4 +27,8 @@ class Setting extends Model
         'api_key',
         'bearer_token',
     ];
+
+    public function getLogoAttribute($value){
+        return isset($value) ? asset("storage/settings/$value") : '';
+    }
 }
